97e5a48a63b1ad25518738796b483c2a9edd6cf9
mohammad_mursalin <mursalinlamon@gmail.com>
2024-03-31 15:02:30 +0600

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+
+    public static void main( String[] args) {
+
+        ProductService service = new ProductService();
+
+        Product p = new Product();
+
+        System.out.println(p);
+    }
+}
---
+++ Product.java
@@ -0,0 +1,15 @@
+class Product {
+
+    private String name;
+    private String type;
+    private String place;
+    private int warranty;
+
+    Product(String name, String type, String place, int warranty) {
+
+        this.name = name;
+        this.type = type;
+        this.place = place;
+        this.warranty = warranty;
+    }
+}
